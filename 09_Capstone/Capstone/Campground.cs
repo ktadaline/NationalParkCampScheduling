96c003d0ea97225db6d53842dae1c421b6fd4889
Rachel Huch <rachelhuch@gmail.com>
2020-02-19 22:12:36 -0500

    ClassesAdded

---
+++ 09_Capstone/Capstone/Campground.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone
+{
+    class Campground
+    {
+        int campgroundId { get; set; }
+        int parkId { get; set; }
+
+        string campgroundName { get; set; }
+
+        int openFromDate { get; set; }//should this be an int? 
+
+        int openToDate { get; set; }
+
+        decimal dailyFee { get; set; }
+
+    }
+}
---
+++ 09_Capstone/Capstone/Park.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone
+{
+    class Park
+    {
+        int parkId { get; set; }
+
+        string parkName { get; set; }
+
+        string location { get; set; }
+
+        string establishDate { get; set; }
+
+        int area { get; set; }
+
+        int visitors { get; set; }
+
+        string description { get; set; }
+
+    } }
+
+
